bdb98c15b4abf7ff74028bbaef613891f4f7c98c
daren <daren22@o2.pl>
2016-12-30 20:11:29 +0000

    after convert to maven project

--- src/com/daren/jersey/Constants.java
+++ src/com/daren/jersey/Constants.java
@@ -2,8 +2,8 @@ package com.daren.jersey;
 
 public class Constants {
     public static String dbClass = "com.mysql.jdbc.Driver";
-    private static String dbName= "surrealist";
-    public static String dbUrl = "jdbc:mysql://eu-cdbr-azure-west-a.cloudapp.net:3306/"+dbName;
-    public static String dbUser = "b39ee9d3d4c166";
-    public static String dbPwd = "a4ffca04";
+    private static String dbName= "surrealist_writer_db";
+    public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
+    public static String dbUser = "root";
+    public static String dbPwd = "";
 }
