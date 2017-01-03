a563f52de8ad39ec0f521853681d6e49dd083d7f
daren <daren22@o2.pl>
2017-01-03 21:08:44 +0000

    change in project for use new librares

--- src/com/daren/jersey/Constants.java
+++ src/com/daren/jersey/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     private static String dbName= "surrealist_writer_db";
     public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
     public static String dbUser = "root";
-    public static String dbPwd = "";
+    public static String dbPwd = "daren";
 }
